9cd1c19fb301c8b62950b7fe6be9d1058dedd783
erdemsert7 <erdemsert89@gmail.com>
2025-05-26 18:20:25 +0300

    Update Tailwind config for Vercel deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,18 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       animation: {
-        fadeIn: 'fadeIn 0.5s ease-in-out',
+        fadeIn: "fadeIn 0.5s ease-in-out",
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
       },
     },
